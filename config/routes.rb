0eb6dfff905f5f5cd73e4fd67c1d5aeb6ff6925c
Erithair <erithair@gmail.com>
2015-06-18 08:35:35 +0000

    Modify routes of recent searches

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Searchjoy::Engine.routes.draw do
   resources :searches, only: [:index] do
     get "overview", on: :collection
+    get "recent", on: :collection
   end
-  get "searches/recent"
   root to: "searches#stream"
 end
